7deca491f4367fd835cafa08c9f776786cff3f3a
Lutfi Haji Cheteh <luthajic@publicisgroupe.net>
2022-10-27 15:39:04 +0200

    fixed routing signin page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import loginImg from "../assets/login.png"
 
 const SignIn = () => {    
+  const navigate = useNavigate();
   return(
-
     <div className="relative w-full h-screen bg-zinc-900/90">
       <img className="absolute w-full h-full object-cover mix-blend-overlay" src={loginImg} alt=""></img>
 
@@ -23,11 +24,11 @@ const SignIn = () => {
 
           <div className="flex justify-between pb-7">
             <p><input className="mx-1 mr-2" type="checkbox" />Remember Me</p>
-            <a href=""><button className="border-none bg-transparent text-black mr-4 hover:text-blue-700">Create an account</button></a>
+            <button className="border-none bg-transparent text-black mr-4 hover:text-blue-700"  onClick={(event) => (navigate("/signup"), event.preventDefault())}>Create an account</button>
           </div>
 
           <button className="border w-full my-5 py-2">Sign In</button>
-          <button className="border w-full mb-5 py-2 bg-slate-500 border-none hover:text-gray-700">Back</button>
+          <button className="border w-full mb-5 py-2 bg-slate-500 border-none hover:text-gray-700" onClick={(event) => (navigate("/"), event.preventDefault())}>Back</button>
         </form>
       </div>
     </div>
